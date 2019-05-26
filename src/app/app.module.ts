3126dbc89fec13c9d7edef378fb7faa1c7afea64
Bruno Morais <brunomoraisnc@gmail.com>
2019-05-26 15:10:47 -0300

    Adiciona função de gps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
 import { TelaPrincipalPage } from '../pages/tela-principal/tela-principal';
-
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -28,6 +29,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/tela-principal/tela-principal.ts
+++ src/pages/tela-principal/tela-principal.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @Component({
   selector: 'page-tela-principal',
@@ -7,7 +9,29 @@ import { NavController } from 'ionic-angular';
 })
 export class TelaPrincipalPage {
 
-  constructor(public navCtrl: NavController) {
+  lat : any;
+  lng : any;
+
+  constructor(public navCtrl: NavController, public geo: Geolocation) {
   }
   
+  getLoc(){
+    this.geo.getCurrentPosition().then( pos => {
+      this.lat = pos.coords.latitude;
+      this.lng = pos.coords.longitude;
+    // resp.coords.latitude
+      // resp.coords.longitude
+     }).catch(err => console.log('Error getting location', err));
+  }
+
+  // getLoc(){
+  //   this.geolocation.getCurrentPosition().then((resp) => {
+  //     // resp.coords.latitude
+  //     // resp.coords.longitude
+  //    }).catch((error) => {
+  //      console.log('Error getting location', error);
+  //    });
+  // }
+  
+
 }
