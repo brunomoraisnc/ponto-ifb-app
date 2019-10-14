4132af414815fceb1073a505639b8e319e4218d9
Bruno Morais <brunomoraisnc@gmail.com>
2019-10-14 17:00:25 -0300

    Remove função de cálculo de presença

--- src/pages/tela-principal/tela-principal.ts
+++ src/pages/tela-principal/tela-principal.ts
@@ -93,39 +93,4 @@ export class TelaPrincipalPage {
       });
       alert.present();
     }
-
-    calcula_distancia(lat1, lon1, lat2, lon2, unit) {
-      /*
-        DEF: Calcula distância entre coordenadas
-        PARAMS:
-            lat1: latitude ponto 1
-            lon1: longitude ponto 1
-            lat2: latitude ponto 2
-            lon2: longitude ponto 2
-            unit: unidade de medida
-      */
-    
-      if ((lat1 == lat2) && (lon1 == lon2)) {
-        return 0;
-      } else {
-        var radlat1 = Math.PI * lat1/180;
-        var radlat2 = Math.PI * lat2/180;
-        var theta = lon1-lon2;
-        var radtheta = Math.PI * theta/180;
-        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        
-        if (dist > 1) {
-          dist = 1;
-        }
-
-        dist = Math.acos(dist);
-        dist = dist * 180/Math.PI;
-        dist = dist * 60 * 1.1515;
-
-        if (unit=="K") { dist = dist * 1.609344 }
-        if (unit=="N") { dist = dist * 0.8684 }
-        
-        return dist;
-      }
-  }
 }
